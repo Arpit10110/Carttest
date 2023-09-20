8bcc14eecac68487148249e5150de90431ffce24
Arpit <omagrahari55@gmail.com>
2023-09-21 01:13:17 +0530

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,7 @@ import menu from "../assets/menu.png"
 import close from "../assets/close.png"
 import { useSelector } from 'react-redux'
 import {BiCart} from"react-icons/bi"
-import { useAuth0 } from "@auth0/auth0-react";
 const Navbar = () => {
-  const { loginWithRedirect,logout , isAuthenticated } = useAuth0();
   const {cartitem} = useSelector(state=>state.cart);
   function menuFun(){
     let menuBtn=document.querySelector(".menu");
@@ -31,9 +29,6 @@ const Navbar = () => {
             <Link onClick={closefun} className='nava'  to="/">Home</Link>
             <Link onClick={closefun} className='nava'  to="/Cart"><BiCart className='carticon'/> <span className='cartval'>{cartitem.length}</span></Link>
             <Link onClick={closefun} className='nava'  to="/Contact">Contact</Link>
-           {
-             isAuthenticated ? (<Link className='nava' onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</Link> ):(<Link className='nava' onClick={() => loginWithRedirect()}>Log In</Link>)
-           }
         </main>
         <img onClick={menuFun}  className="menu" src={menu} alt="" />
     </nav>
--- src/index.js
+++ src/index.js
@@ -2,19 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from  "react-redux";
-import { Auth0Provider } from '@auth0/auth0-react';
 import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Auth0Provider
-    domain="dev-fwulh6r8i0r0xpmr.us.auth0.com"
-    clientId="RcZ9m8W4Awl3HINgJi8hocPwahrHx0te"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
   <Provider store={store}>
   <App/>
 </Provider>
-</Auth0Provider>
 );
