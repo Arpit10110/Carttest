536ef20919493ed8ded9653990e740fd3e3b7f43
Arpit <omagrahari55@gmail.com>
2023-09-21 02:39:33 +0530

    fifth commit

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from  "react-redux";
 import store from './redux/store';
+import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Auth0Provider
+  domain="dev-fwulh6r8i0r0xpmr.us.auth0.com"
+  clientId="vCg9jjrQcLP0wJQSUM7g20X9eCuX4ESW"
+  authorizationParams={{
+    redirect_uri: window.location.origin
+  }}>
   <Provider store={store}>
   <App/>
 </Provider>
+</Auth0Provider>
 );
